72b88d74b5411e9b0a3bd25e5a4c031f892780a8
Timofey-Kolchedantsev <timoha235472@gmail.com>
2023-06-12 18:30:35 +0300

    initail

---
+++ lesson1.py
@@ -0,0 +1,22 @@
+# def strcounter(s):
+#     for sym in set(s):
+#         counter = 0
+#         for sub_sym in s:
+#             if sym == sub_sym:
+#                 counter += 1
+#         print(sym, counter)
+
+# strcounter('aab')
+
+
+
+
+def strcounter(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+    for sym, count in syms_counter.items():
+        print(sym, count)
+
+strcounter('wqghbjklwhgouwiqhngrwoiuvn qo l;k,ijoy4jthe3xr')
